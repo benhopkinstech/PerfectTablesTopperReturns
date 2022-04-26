9eff842f990353de4385287c500f366dd3c62d6a
Ben Hopkins <13667722+benhopkinstech@users.noreply.github.com>
2022-04-26 21:01:22 +0100

    Added a data grid view and a 'Search the returns' section with buttons for features to be implemented. A button labelled 'View All Returns' now populates the data grid view with all the data in the database

--- PerfectTablesTopperReturns/Form1.Designer.cs
+++ PerfectTablesTopperReturns/Form1.Designer.cs
@@ -39,13 +39,30 @@
             this.btnAddReturn = new System.Windows.Forms.Button();
             this.txtNumber = new System.Windows.Forms.Label();
             this.numNumber = new System.Windows.Forms.NumericUpDown();
+            this.txtSearchReturns = new System.Windows.Forms.Label();
+            this.numNumberS = new System.Windows.Forms.NumericUpDown();
+            this.txtNumberS = new System.Windows.Forms.Label();
+            this.txtColourS = new System.Windows.Forms.Label();
+            this.txtDesignS = new System.Windows.Forms.Label();
+            this.txtSizeS = new System.Windows.Forms.Label();
+            this.cmbColourS = new System.Windows.Forms.ComboBox();
+            this.cmbDesignS = new System.Windows.Forms.ComboBox();
+            this.cmbSizeS = new System.Windows.Forms.ComboBox();
+            this.dataFound = new System.Windows.Forms.DataGridView();
+            this.btnViewAll = new System.Windows.Forms.Button();
+            this.btnSearchSize = new System.Windows.Forms.Button();
+            this.btnSearchDesign = new System.Windows.Forms.Button();
+            this.btnSearchColour = new System.Windows.Forms.Button();
+            this.btnSearchNumber = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.numNumber)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numNumberS)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataFound)).BeginInit();
             this.SuspendLayout();
             // 
             // txtAddReturn
             // 
             this.txtAddReturn.AutoSize = true;
-            this.txtAddReturn.Location = new System.Drawing.Point(12, 179);
+            this.txtAddReturn.Location = new System.Drawing.Point(5, 175);
             this.txtAddReturn.Name = "txtAddReturn";
             this.txtAddReturn.Size = new System.Drawing.Size(73, 15);
             this.txtAddReturn.TabIndex = 0;
@@ -62,7 +79,7 @@
             "6 x 5-6cm Wide",
             "12cm Wide",
             "15cm Wide"});
-            this.cmbSize.Location = new System.Drawing.Point(60, 197);
+            this.cmbSize.Location = new System.Drawing.Point(61, 193);
             this.cmbSize.Name = "cmbSize";
             this.cmbSize.Size = new System.Drawing.Size(121, 23);
             this.cmbSize.TabIndex = 1;
@@ -78,7 +95,7 @@
             "Groovy",
             "Ornate",
             "Crown Curly"});
-            this.cmbDesign.Location = new System.Drawing.Point(60, 226);
+            this.cmbDesign.Location = new System.Drawing.Point(61, 222);
             this.cmbDesign.Name = "cmbDesign";
             this.cmbDesign.Size = new System.Drawing.Size(121, 23);
             this.cmbDesign.TabIndex = 2;
@@ -126,7 +143,7 @@
             "Green Mirror ST072",
             "Rose Gold Mirror ST074",
             "Pink Mirror ST075"});
-            this.cmbColour.Location = new System.Drawing.Point(60, 255);
+            this.cmbColour.Location = new System.Drawing.Point(61, 251);
             this.cmbColour.Name = "cmbColour";
             this.cmbColour.Size = new System.Drawing.Size(121, 23);
             this.cmbColour.TabIndex = 3;
@@ -134,7 +151,7 @@
             // txtSize
             // 
             this.txtSize.AutoSize = true;
-            this.txtSize.Location = new System.Drawing.Point(27, 200);
+            this.txtSize.Location = new System.Drawing.Point(28, 196);
             this.txtSize.Name = "txtSize";
             this.txtSize.Size = new System.Drawing.Size(27, 15);
             this.txtSize.TabIndex = 4;
@@ -143,7 +160,7 @@
             // txtDesign
             // 
             this.txtDesign.AutoSize = true;
-            this.txtDesign.Location = new System.Drawing.Point(16, 229);
+            this.txtDesign.Location = new System.Drawing.Point(17, 225);
             this.txtDesign.Name = "txtDesign";
             this.txtDesign.Size = new System.Drawing.Size(43, 15);
             this.txtDesign.TabIndex = 5;
@@ -152,7 +169,7 @@
             // txtColour
             // 
             this.txtColour.AutoSize = true;
-            this.txtColour.Location = new System.Drawing.Point(12, 258);
+            this.txtColour.Location = new System.Drawing.Point(13, 254);
             this.txtColour.Name = "txtColour";
             this.txtColour.Size = new System.Drawing.Size(43, 15);
             this.txtColour.TabIndex = 6;
@@ -160,9 +177,9 @@
             // 
             // btnAddReturn
             // 
-            this.btnAddReturn.Location = new System.Drawing.Point(106, 313);
+            this.btnAddReturn.Location = new System.Drawing.Point(62, 310);
             this.btnAddReturn.Name = "btnAddReturn";
-            this.btnAddReturn.Size = new System.Drawing.Size(75, 23);
+            this.btnAddReturn.Size = new System.Drawing.Size(120, 23);
             this.btnAddReturn.TabIndex = 7;
             this.btnAddReturn.Text = "Add Return";
             this.btnAddReturn.UseVisualStyleBackColor = true;
@@ -171,7 +188,7 @@
             // txtNumber
             // 
             this.txtNumber.AutoSize = true;
-            this.txtNumber.Location = new System.Drawing.Point(4, 286);
+            this.txtNumber.Location = new System.Drawing.Point(5, 282);
             this.txtNumber.Name = "txtNumber";
             this.txtNumber.Size = new System.Drawing.Size(51, 15);
             this.txtNumber.TabIndex = 8;
@@ -179,16 +196,218 @@
             // 
             // numNumber
             // 
-            this.numNumber.Location = new System.Drawing.Point(61, 284);
+            this.numNumber.Location = new System.Drawing.Point(62, 280);
             this.numNumber.Name = "numNumber";
             this.numNumber.Size = new System.Drawing.Size(120, 23);
             this.numNumber.TabIndex = 10;
+            // 
+            // txtSearchReturns
+            // 
+            this.txtSearchReturns.AutoSize = true;
+            this.txtSearchReturns.Location = new System.Drawing.Point(5, 32);
+            this.txtSearchReturns.Name = "txtSearchReturns";
+            this.txtSearchReturns.Size = new System.Drawing.Size(102, 15);
+            this.txtSearchReturns.TabIndex = 11;
+            this.txtSearchReturns.Text = "Search the returns";
+            // 
+            // numNumberS
+            // 
+            this.numNumberS.Location = new System.Drawing.Point(62, 137);
+            this.numNumberS.Name = "numNumberS";
+            this.numNumberS.Size = new System.Drawing.Size(120, 23);
+            this.numNumberS.TabIndex = 20;
+            // 
+            // txtNumberS
+            // 
+            this.txtNumberS.AutoSize = true;
+            this.txtNumberS.Location = new System.Drawing.Point(5, 139);
+            this.txtNumberS.Name = "txtNumberS";
+            this.txtNumberS.Size = new System.Drawing.Size(51, 15);
+            this.txtNumberS.TabIndex = 19;
+            this.txtNumberS.Text = "Number";
+            // 
+            // txtColourS
+            // 
+            this.txtColourS.AutoSize = true;
+            this.txtColourS.Location = new System.Drawing.Point(13, 111);
+            this.txtColourS.Name = "txtColourS";
+            this.txtColourS.Size = new System.Drawing.Size(43, 15);
+            this.txtColourS.TabIndex = 17;
+            this.txtColourS.Text = "Colour";
+            // 
+            // txtDesignS
+            // 
+            this.txtDesignS.AutoSize = true;
+            this.txtDesignS.Location = new System.Drawing.Point(17, 82);
+            this.txtDesignS.Name = "txtDesignS";
+            this.txtDesignS.Size = new System.Drawing.Size(43, 15);
+            this.txtDesignS.TabIndex = 16;
+            this.txtDesignS.Text = "Design";
+            // 
+            // txtSizeS
+            // 
+            this.txtSizeS.AutoSize = true;
+            this.txtSizeS.Location = new System.Drawing.Point(28, 53);
+            this.txtSizeS.Name = "txtSizeS";
+            this.txtSizeS.Size = new System.Drawing.Size(27, 15);
+            this.txtSizeS.TabIndex = 15;
+            this.txtSizeS.Text = "Size";
+            // 
+            // cmbColourS
+            // 
+            this.cmbColourS.FormattingEnabled = true;
+            this.cmbColourS.Items.AddRange(new object[] {
+            "Pure White ST046",
+            "White/Iridescent ST028",
+            "Ivory ST048",
+            "Champagne ST040",
+            "Gold ST035",
+            "Silver ST034",
+            "Pewter ST042",
+            "Brown ST049",
+            "Black ST036",
+            "Rose Gold ST043",
+            "Pale Pink ST029",
+            "Rose Pink ST039",
+            "Cerise ST037",
+            "Neon Pink ST058",
+            "Lilac ST031",
+            "Purple ST038",
+            "Bright Yellow ST054",
+            "Peach ST059",
+            "Orange ST050",
+            "Red ST032",
+            "Burgundy ST053",
+            "Icy Blue ST047",
+            "Blue ST030",
+            "Celeste ST056",
+            "Sapphire ST051",
+            "Royal Blue ST060",
+            "Navy ST041",
+            "Mint ST057",
+            "Light Green ST045",
+            "Green ST033",
+            "Turquoise ST044",
+            "Bright Rainbow ST055",
+            "Pastel Mix ST052",
+            "Silver Mirror ST071",
+            "Gold Mirror ST070",
+            "Red Mirror ST073",
+            "Green Mirror ST072",
+            "Rose Gold Mirror ST074",
+            "Pink Mirror ST075"});
+            this.cmbColourS.Location = new System.Drawing.Point(61, 108);
+            this.cmbColourS.Name = "cmbColourS";
+            this.cmbColourS.Size = new System.Drawing.Size(121, 23);
+            this.cmbColourS.TabIndex = 14;
+            // 
+            // cmbDesignS
+            // 
+            this.cmbDesignS.FormattingEnabled = true;
+            this.cmbDesignS.Items.AddRange(new object[] {
+            "Block",
+            "Bold",
+            "Bubble",
+            "Curly",
+            "Groovy",
+            "Ornate",
+            "Crown Curly"});
+            this.cmbDesignS.Location = new System.Drawing.Point(61, 79);
+            this.cmbDesignS.Name = "cmbDesignS";
+            this.cmbDesignS.Size = new System.Drawing.Size(121, 23);
+            this.cmbDesignS.TabIndex = 13;
+            // 
+            // cmbSizeS
+            // 
+            this.cmbSizeS.FormattingEnabled = true;
+            this.cmbSizeS.Items.AddRange(new object[] {
+            "6 x 3.5cm Tall",
+            "5cm Tall",
+            "7cm Tall",
+            "10cm Tall",
+            "6 x 5-6cm Wide",
+            "12cm Wide",
+            "15cm Wide"});
+            this.cmbSizeS.Location = new System.Drawing.Point(61, 50);
+            this.cmbSizeS.Name = "cmbSizeS";
+            this.cmbSizeS.Size = new System.Drawing.Size(121, 23);
+            this.cmbSizeS.TabIndex = 12;
+            // 
+            // dataFound
+            // 
+            this.dataFound.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataFound.Location = new System.Drawing.Point(305, 29);
+            this.dataFound.Name = "dataFound";
+            this.dataFound.RowTemplate.Height = 25;
+            this.dataFound.Size = new System.Drawing.Size(470, 307);
+            this.dataFound.TabIndex = 21;
+            // 
+            // btnViewAll
+            // 
+            this.btnViewAll.Location = new System.Drawing.Point(658, 342);
+            this.btnViewAll.Name = "btnViewAll";
+            this.btnViewAll.Size = new System.Drawing.Size(117, 23);
+            this.btnViewAll.TabIndex = 22;
+            this.btnViewAll.Text = "View All Returns";
+            this.btnViewAll.UseVisualStyleBackColor = true;
+            this.btnViewAll.Click += new System.EventHandler(this.btnViewAll_Click);
+            // 
+            // btnSearchSize
+            // 
+            this.btnSearchSize.Location = new System.Drawing.Point(188, 50);
+            this.btnSearchSize.Name = "btnSearchSize";
+            this.btnSearchSize.Size = new System.Drawing.Size(100, 23);
+            this.btnSearchSize.TabIndex = 23;
+            this.btnSearchSize.Text = "Search Size";
+            this.btnSearchSize.UseVisualStyleBackColor = true;
+            // 
+            // btnSearchDesign
+            // 
+            this.btnSearchDesign.Location = new System.Drawing.Point(188, 79);
+            this.btnSearchDesign.Name = "btnSearchDesign";
+            this.btnSearchDesign.Size = new System.Drawing.Size(100, 23);
+            this.btnSearchDesign.TabIndex = 24;
+            this.btnSearchDesign.Text = "Search Design";
+            this.btnSearchDesign.UseVisualStyleBackColor = true;
+            // 
+            // btnSearchColour
+            // 
+            this.btnSearchColour.Location = new System.Drawing.Point(188, 108);
+            this.btnSearchColour.Name = "btnSearchColour";
+            this.btnSearchColour.Size = new System.Drawing.Size(100, 23);
+            this.btnSearchColour.TabIndex = 25;
+            this.btnSearchColour.Text = "Search Colour";
+            this.btnSearchColour.UseVisualStyleBackColor = true;
+            // 
+            // btnSearchNumber
+            // 
+            this.btnSearchNumber.Location = new System.Drawing.Point(188, 137);
+            this.btnSearchNumber.Name = "btnSearchNumber";
+            this.btnSearchNumber.Size = new System.Drawing.Size(100, 23);
+            this.btnSearchNumber.TabIndex = 26;
+            this.btnSearchNumber.Text = "Search Number";
+            this.btnSearchNumber.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(787, 373);
+            this.Controls.Add(this.btnSearchNumber);
+            this.Controls.Add(this.btnSearchColour);
+            this.Controls.Add(this.btnSearchDesign);
+            this.Controls.Add(this.btnSearchSize);
+            this.Controls.Add(this.btnViewAll);
+            this.Controls.Add(this.dataFound);
+            this.Controls.Add(this.numNumberS);
+            this.Controls.Add(this.txtNumberS);
+            this.Controls.Add(this.txtColourS);
+            this.Controls.Add(this.txtDesignS);
+            this.Controls.Add(this.txtSizeS);
+            this.Controls.Add(this.cmbColourS);
+            this.Controls.Add(this.cmbDesignS);
+            this.Controls.Add(this.cmbSizeS);
+            this.Controls.Add(this.txtSearchReturns);
             this.Controls.Add(this.numNumber);
             this.Controls.Add(this.txtNumber);
             this.Controls.Add(this.btnAddReturn);
@@ -204,6 +423,8 @@
             this.Name = "Form1";
             this.Text = "Perfect Tables Topper Returns";
             ((System.ComponentModel.ISupportInitialize)(this.numNumber)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numNumberS)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataFound)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -221,5 +442,20 @@
         private Button btnAddReturn;
         private Label txtNumber;
         private NumericUpDown numNumber;
+        private Label txtSearchReturns;
+        private NumericUpDown numNumberS;
+        private Label txtNumberS;
+        private Label txtColourS;
+        private Label txtDesignS;
+        private Label txtSizeS;
+        private ComboBox cmbColourS;
+        private ComboBox cmbDesignS;
+        private ComboBox cmbSizeS;
+        private DataGridView dataFound;
+        private Button btnViewAll;
+        private Button btnSearchSize;
+        private Button btnSearchDesign;
+        private Button btnSearchColour;
+        private Button btnSearchNumber;
     }
 }
--- PerfectTablesTopperReturns/Form1.cs
+++ PerfectTablesTopperReturns/Form1.cs
@@ -1,3 +1,4 @@
+using System.Data;
 using System.Data.SqlClient;
 
 namespace PerfectTablesTopperReturns
@@ -31,6 +32,25 @@ namespace PerfectTablesTopperReturns
             else
             {
                 MessageBox.Show("FAILED: Return not added");
+            }
+            con.Close();
+        }
+
+        private void btnSearchReturns_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnViewAll_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\benho\source\repos\PerfectTablesTopperReturns\PerfectTablesTopperReturns\db.mdf;Integrated Security=True");
+            con.Open();
+            string query = "select Size, Design, Colour, Number from Returns";
+            using (var adapter = new SqlDataAdapter(query, con))
+            {
+                var table = new DataTable();
+                adapter.Fill(table);
+                dataFound.DataSource = table;
             }
             con.Close();
         }
